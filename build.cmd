48d1ed2d3b7f7eca58e261190bb63219b6a21c3c
Jeffrey Pratt <jeff@metronomix.com>
2011-01-31 13:17:30 -0700

    Cosmetic change to build script.

--- build.cmd
+++ build.cmd
@@ -4,15 +4,15 @@ set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319\
 
 if not exist output ( mkdir output ) else del /q output\*.*
 
-echo Compiling
+echo Compiling...
 msbuild /nologo /verbosity:quiet src/FileStorageAdapter.sln /p:Configuration=Release /t:Clean
 msbuild /nologo /verbosity:quiet src/FileStorageAdapter.sln /p:Configuration=Release
 
-echo Copying
+echo Copying...
 copy src\proj\FileStorageAdapter\bin\Release\*.* output\
 copy src\proj\FileStorageAdapter.AmazonS3\bin\Release\*.* output\
 
-echo Cleaning
+echo Cleaning...
 msbuild /nologo /verbosity:quiet src/FileStorageAdapter.sln /p:Configuration=Release /t:Clean
 
-echo Done
+echo Done.
