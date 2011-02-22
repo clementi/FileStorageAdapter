b456538a5d3006f247e9074c47e73ec5a4f18c4f
Jeffrey Pratt <jeff@metronomix.com>
2011-02-22 09:37:01 -0700

    Added ILMerge option to build.cmd. Usage: build.cmd merge

--- build.cmd
+++ build.cmd
@@ -1,8 +1,9 @@
 @echo off
 
-set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319\
+set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319\;bin
 
 if not exist output ( mkdir output ) else del /q output\*.*
+if exist output\merged ( rd /s /q output\merged )
 
 echo Compiling...
 msbuild /nologo /verbosity:quiet src/FileStorageAdapter.sln /p:Configuration=Release /t:Clean
@@ -12,6 +13,12 @@ echo Copying...
 copy src\proj\FileStorageAdapter\bin\Release\*.* output\
 copy src\proj\FileStorageAdapter.AmazonS3\bin\Release\*.* output\
 
+if not "%1"=="merge" ( goto clean )
+if not exist output\merged ( mkdir output\merged )
+echo Merging...
+ilmerge /keyfile:src\FileStorageAdapter.snk /internalize:exclude.txt /targetplatform:v4,c:\Windows\Microsoft.NET\Framework\v4.0.30319 /out:output\merged\FileStorageAdapter.dll output\FileStorageAdapter.dll output\FileStorageAdapter.AmazonS3.dll output\AWSSDK.dll
+
+:clean
 echo Cleaning...
 msbuild /nologo /verbosity:quiet src/FileStorageAdapter.sln /p:Configuration=Release /t:Clean
 
