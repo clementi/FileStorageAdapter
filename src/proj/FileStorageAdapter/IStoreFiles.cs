92238fd1e66a6883b42f359c778093a93be9a7a2
Whatcott, Michael <mdwhatcott@gmail.com>
2011-06-30 22:35:29 -0600

    Removed an overly specific method from the IStoreFiles interface.

--- src/proj/FileStorageAdapter/IStoreFiles.cs
+++ src/proj/FileStorageAdapter/IStoreFiles.cs
@@ -7,7 +7,6 @@ namespace FileStorageAdapter
 	public interface IStoreFiles
 	{
 		Stream Get(string path);
-		string GetPreSignedUrl(string path, DateTime expiration);
 		void Put(Stream input, string path);
 		void Delete(string path);
 		IEnumerable<string> EnumerateObjects(string location);
